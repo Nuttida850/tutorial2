dd26c3abc6c3cbf7d23164c9ed58d4c3f3c2d477
mirei <ovamirei@gmail.com>
2025-05-29 10:17:34 +0700

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Search
 let search = document.getElementById("search");
 let main_search = document.getElementById("main-search");
 
@@ -5,6 +6,7 @@ search.addEventListener("click", () => {
   main_search.classList.toggle("active");
 });
 
+// set darktheme
 let theme = document.getElementById("theme");
 
 theme.addEventListener("click", () => {
@@ -17,9 +19,25 @@ theme.addEventListener("click", () => {
   }
 });
 
+// bar menu
 let bar = document.getElementById("bar");
 let nav_links = document.querySelector(".nav-links");
 
 bar.addEventListener("click", () => {
   nav_links.classList.toggle("active");
+});
+
+let scroll_down = document.getElementById("scroll-down");
+
+document.addEventListener("scroll", () => {
+  let windowY = window.scrollY;
+  if (windowY > 60) {
+    scroll_down.style.display = "flex";
+  } else {
+    scroll_down.style.display = "none";
+  }
+});
+
+scroll_down.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
 });
